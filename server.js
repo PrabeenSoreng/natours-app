85e6e7a14c1efa468da7b53d8b5c31c137d39402
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2020-01-22 10:47:05 +0530

    database connection successful

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({
   path: "./config.env"
 });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log("DB connection successful...");
+  })
+  .catch(err => console.log(err));
 
 const app = require("./index.js");
 // console.log(app.get("env"));
