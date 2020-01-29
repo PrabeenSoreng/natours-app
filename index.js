47e88b5b1c337954953aa145349d40dc4526167b
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2020-01-29 12:15:04 +0530

    global error handling implemented

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError.js");
+const globalErrorHandler = require("./controllers/errorController.js");
 const tourRouter = require("./routes/tourRoutes.js");
 const userRouter = require("./routes/userRoutes.js");
 
@@ -15,5 +17,15 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server!!!`
+  // });
+  next(new AppError(`Can't find ${req.originalUrl} on this server!!!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
