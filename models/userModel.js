dee4766acd089873b871d5e8300e8f89b8b15c7f
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2020-01-30 23:24:41 +0530

    signup and login added with jwt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,12 +22,34 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, "Password is required."],
-    minLength: 8
+    minLength: 8,
+    select: false
   },
   confirmPassword: {
     type: String,
-    required: [true, "Confirm password is requred."]
+    required: [true, "Confirm password is requred."],
+    validate: {
+      // This only works on save and create.
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same!"
+    }
   }
 });
 
-module.exports = mongoose.Model("User", userSchema);
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+module.exports = mongoose.model("User", userSchema);
