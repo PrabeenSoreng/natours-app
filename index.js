b9d21235f77e0f68d199daefdf691ff377409163
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2020-07-11 19:30:45 +0530

    improved stripe implementation

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const tourRouter = require("./routes/tourRoutes.js");
 const userRouter = require("./routes/userRoutes.js");
 const reviewRouter = require("./routes/reviewRoutes.js");
 const bookingRouter = require("./routes/bookingRoutes.js");
+const bookingController = require("./controllers/bookingController.js");
 const viewRouter = require("./routes/viewRoute.js");
 
 const app = express();
@@ -29,6 +30,7 @@ app.set("views", path.join(__dirname, "views"));
 
 // Implement cors
 app.use(cors());
+app.options("*", cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, "public")));
@@ -49,6 +51,12 @@ const limiter = rateLimit({
   message: "Too many requests from this IP, please try again in an hour...",
 });
 app.use("/api", limiter);
+
+app.post(
+  "/webhook-checkout",
+  express.raw({ type: "application/json" }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
